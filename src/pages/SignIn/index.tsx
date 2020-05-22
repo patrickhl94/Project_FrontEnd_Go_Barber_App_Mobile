2af970d67bb5331054c962205ae6bbfd3bb0459d
Patrick Lima <patrickhl94@hotmail.com>
2020-05-21 23:50:43 -0300

    Created files Pages

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, StatusBar } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <StatusBar backgroundColor="#312e38" barStyle="light-content" />
+      <Text>App</Text>
+    </Container>
+  );
+};
+
+export default SignIn;
