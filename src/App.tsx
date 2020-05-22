3532a9dfcdb439edc7f090bc87e3a7f68e53500f
Patrick Lima <patrickhl94@hotmail.com>
2020-05-21 23:15:18 -0300

    Created App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
