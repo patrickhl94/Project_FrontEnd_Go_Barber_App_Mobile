a05ff77ecddbf4dbbd7655b44fecef5a93643165
Patrick Lima <patrickhl94@hotmail.com>
2020-05-22 22:26:05 -0300

    Update page SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Text, StatusBar } from 'react-native';
+import { Text } from 'react-native';
 
 import { Container } from './styles';
 
 const SignUp: React.FC = () => {
   return (
     <Container style={{ backgroundColor: '#312e38', flex: 1 }}>
-      <StatusBar backgroundColor="#312e38" barStyle="light-content" />
       <Text>App</Text>
     </Container>
   );
