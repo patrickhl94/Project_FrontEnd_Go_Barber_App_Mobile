48fe5ecae34389c97d624fdada2f4342eddf08e8
Patrick Lima <patrickhl94@hotmail.com>
2020-06-04 09:38:49 -0300

    Updated page SignIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,7 +6,7 @@ import logoImage from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import { Container, Title, ForgotPassword, ForgotPasswordText } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -18,6 +18,10 @@ const SignIn: React.FC = () => {
       <Input name="password" icon="lock" placeholder="Senha" />
 
       <Button>Entrar</Button>
+
+      <ForgotPassword onPress={() => {}}>
+        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+      </ForgotPassword>
     </Container>
   );
 };
