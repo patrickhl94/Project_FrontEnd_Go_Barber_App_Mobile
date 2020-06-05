7480fdebd766e4f9a9c2bab1d658e48c2118a63e
Patrick Lima <patrickhl94@hotmail.com>
2020-06-05 11:09:20 -0300

    Updated component Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -8,11 +9,43 @@ interface InputProps extends TextInputProps {
   icon: string;
 }
 
+interface InputValueReference {
+  value: string;
+}
+
 const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+  const InputElementRef = useRef<any>(null);
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      setValue(ref: any, value: string) {
+        inputValueRef.current.value = value;
+        InputElementRef.current.setNativeProps({ text: value });
+      },
+      clearValue() {
+        inputValueRef.current.value = '';
+        InputElementRef.current.clear();
+      },
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container>
       <Icon name={icon} size={20} color="#666360" />
-      <TextInput placeholderTextColor="#666360" {...rest} />
+      <TextInput
+        ref={InputElementRef}
+        placeholderTextColor="#666360"
+        {...rest}
+        defaultValue={defaultValue}
+        onChangeText={value => {
+          inputValueRef.current.value = value;
+        }}
+      />
     </Container>
   );
 };
