522582a523dbc441f5a6fe7de5a797f9cffc8932
Patrick Lima <patrickhl94@hotmail.com>
2020-06-05 10:18:09 -0300

    Updted page SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,13 +1,63 @@
 import React from 'react';
-import { Text } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import logoImage from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInButtonText,
+} from './styles';
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
-    <Container style={{ backgroundColor: '#312e38', flex: 1 }}>
-      <Text>App</Text>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          contentContainerStyle={
+            Platform.OS === 'android' ? { flex: 0 } : { flex: 1 }
+          }
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImage} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignInButton onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInButtonText>Voltar Login</BackToSignInButtonText>
+      </BackToSignInButton>
+    </>
   );
 };
 
