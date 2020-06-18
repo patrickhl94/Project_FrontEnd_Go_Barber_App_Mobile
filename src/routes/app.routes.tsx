23d50abd678b896b28538a890a9bceef97e2b492
Patrick Lima <patrickhl94@hotmail.com>
2020-06-18 11:42:13 -0300

    Created routes App

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Dashboard from '../pages/Dashboard';
+
+const Routes: React.FC = () => {
+  const App = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <App.Navigator
+        screenOptions={{
+          // headerShown: false,
+          cardStyle: { backgroundColor: '#312e38' },
+        }}
+      >
+        <App.Screen name="Dashboard" component={Dashboard} />
+      </App.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
