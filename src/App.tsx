6dcdc3ff23efe9aa5f79f4c3f5317240c8bdbce1
Patrick Lima <patrickhl94@hotmail.com>
2020-06-18 11:41:42 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 
+import AppProvider from './hooks';
 import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-      <StatusBar backgroundColor="#312e38" barStyle="light-content" />
-      <Routes />
-    </View>
+    <AppProvider>
+      <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+        <StatusBar backgroundColor="#312e38" barStyle="light-content" />
+        <Routes />
+      </View>
+    </AppProvider>
   );
 };
 
