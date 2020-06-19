3774d38fce35de85e8e5414f2c2df5b621f3f16d
Patrick Lima <patrickhl94@hotmail.com>
2020-06-18 21:54:12 -0300

    Updated page Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, View } from 'react-native';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  return <Text>Dashboard</Text>;
+  const { signOut } = useAuth();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingBottom: 15,
+        paddingHorizontal: 15,
+      }}
+    >
+      <Button color="#999" title="Sair" onPress={signOut} />
+    </View>
+  );
 };
 
 export default Dashboard;
