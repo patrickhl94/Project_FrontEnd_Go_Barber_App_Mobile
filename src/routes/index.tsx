aa8963a288a2148e9ba8cfaa3b2f67508a06392c
Patrick Lima <patrickhl94@hotmail.com>
2020-06-18 21:54:29 -0300

    Updated indox router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
+import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
@@ -7,7 +7,15 @@ import AppRoutes from './app.routes';
 import { useAuth } from '../hooks/auth';
 
 const Routes: React.FC = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#999" />
+      </View>
+    );
+  }
 
   return user ? <AppRoutes /> : <AuthRoutes />;
 };
